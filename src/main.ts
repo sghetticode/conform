1b4700c86ce7317ba883f11dc8679efd141a2717
jhb <yourstruthy@protonmail.com>
2026-06-19 22:36:17 -0600

    Add ready button logging

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,10 @@
 import './style.css'
 
 console.log('Starting up Conform...')
+
+// Start trait test
+const readyBtn = document.getElementById('ready-btn')
+
+readyBtn?.addEventListener('click', () => {
+  console.log('Ready to start.')
+})
