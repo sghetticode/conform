3db39cfe10bc2f2956bd30e4acb13415705ae363
jhb <yourstruthy@protonmail.com>
2026-08-19 18:06:57 -0600

    Add COOP & COEP headers

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  optimizeDeps: {
+    exclude: ['@huggingface/transformers'],
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
+  },
 })
